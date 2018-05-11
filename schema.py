d505619752bea9a5f077408bef5c9ded5c3a92f9
Oliver Kaiser <ok@ong.at>
2018-05-11 15:22:43 +0200

    added json schema for payload validation

---
+++ schema.py
@@ -0,0 +1,10 @@
+network_object = {
+    "title": "network_object",
+    "type": "object",
+    "properties": {
+        "id": {
+            "description": "auto-generated unique identifier of api object",
+            "type": "string"
+        }
+    }
+}
