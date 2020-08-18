64b5876cca5b50083f806b0369172662401e3e2e
Oliver Kaiser <ok@ong.at>
2020-08-18 19:30:31 +0200

    added tests for object crud operations

--- test/test_fireREST.py
+++ test/test_fireREST.py
@@ -22,3 +22,39 @@ def test_initialization(api, constants):
     assert api.verify_cert == expected_verify_cert
     assert api.timeout == expected_timeout
     assert api.domain_name == expected_domain_name
+
+
+def test_create_object(api):
+    payload = {
+        'name': 'firerest_test_netobj',
+        'value': '198.18.0.0/24',
+    }
+
+    actual_result = api.create_object('network', payload).status_code
+    expected_result = 201
+
+    assert expected_result == actual_result
+
+
+def test_update_object(api):
+    expected_response = 200
+    expected_description = 'test_update_object'
+
+    object_id = api.get_object_id_by_name('network', 'firerest_test_netobj')
+    payload = api.get_object('network', object_id)
+    payload['description'] = expected_description
+
+    actual_response = api.update_object('network', object_id, payload).status_code
+    actual_description = api.get_object('network', object_id)['description']
+
+    assert expected_response == actual_response
+    assert expected_description == actual_description
+
+
+def test_delete_object(api):
+    object_id = api.get_object_id_by_name('network', 'firerest_test_netobj')
+
+    actual_result = api.delete_object('network', object_id).status_code
+    expected_result = 200
+
+    assert expected_result == actual_result
