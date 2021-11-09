b20d721c229194f15f255bab18ef5befd540837b
Daniel Heule <daniel.heule@gmail.com>
2021-11-09 11:07:02 +0100

    Fix for dynamicobject.mapping (#57)

--- fireREST/fmc/object/dynamicobject/mapping/__init__.py
+++ fireREST/fmc/object/dynamicobject/mapping/__init__.py
@@ -4,7 +4,7 @@ from fireREST.fmc import ChildResource
 class Mapping(ChildResource):
     CONTAINER_NAME = 'DynamicObject'
     CONTAINER_PATH = '/object/dynamicobjects/{uuid}'
-    PATH = '/object/dynamicobjects/{container_uuid}/overrides/{uuid}'
+    PATH = '/object/dynamicobjects/{container_uuid}/mappings'
     MINIMUM_VERSION_REQUIRED_GET = '7.0.0'
     MINIMUM_VERSION_REQUIRED_UPDATE = '7.0.0'
     MINIMUM_VERSION_REQUIRED_DELETE = '7.0.0'
