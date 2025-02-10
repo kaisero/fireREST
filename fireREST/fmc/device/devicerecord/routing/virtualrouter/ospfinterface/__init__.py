a932ec5d328b9f5559ecc109e3b1edb1abfade01
rchrabas <rchrabas@cisco.com>
2025-02-10 13:50:06 +0100

    Move to ruff and poetry

--- fireREST/fmc/device/devicerecord/routing/virtualrouter/ospfinterface/__init__.py
+++ fireREST/fmc/device/devicerecord/routing/virtualrouter/ospfinterface/__init__.py
@@ -7,7 +7,5 @@ class OspfInterface(NestedChildResource):
     CONTAINER_PATH = '/devices/devicerecords/{uuid}'
     CHILD_CONTAINER_NAME = 'VirtualRouter'
     CHILD_CONTAINER_PATH = '/devices/devicerecords/{container_uuid}/routing/virtualrouters/{uuid}'
-    PATH = (
-        '/devices/devicerecords/{container_uuid}/routing/virtualrouters/{child_container_uuid}/' 'ospfinterface/{uuid}'
-    )
+    PATH = '/devices/devicerecords/{container_uuid}/routing/virtualrouters/{child_container_uuid}/ospfinterface/{uuid}'
     MINIMUM_VERSION_REQUIRED_GET = API_RELEASE_660
--- fireREST/fmc/device/devicerecord/routing/virtualrouter/policybasedroute/__init__.py
+++ fireREST/fmc/device/devicerecord/routing/virtualrouter/policybasedroute/__init__.py
@@ -8,8 +8,7 @@ class PolicyBasedRoute(NestedChildResource):
     CHILD_CONTAINER_NAME = 'VirtualRouter'
     CHILD_CONTAINER_PATH = '/devices/devicerecords/{container_uuid}/routing/virtualrouters/{uuid}'
     PATH = (
-        '/devices/devicerecords/{container_uuid}/routing/virtualrouters/{child_container_uuid}/'
-        'policybasedroutes/{uuid}'
+        '/devices/devicerecords/{container_uuid}/routing/virtualrouters/{child_container_uuid}/policybasedroutes/{uuid}'
     )
     MINIMUM_VERSION_REQUIRED_CREATE = API_RELEASE_710
     MINIMUM_VERSION_REQUIRED_GET = API_RELEASE_710
